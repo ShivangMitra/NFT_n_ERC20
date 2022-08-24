18b4f721dccaecd65f45ee6b5492da461330b6b6
Shivang Mitra <shivangmitra8@gmail.com>
2022-08-24 16:14:53 +0530

    Completed

--- contracts/Krion.sol
+++ contracts/Krion.sol
@@ -2,15 +2,22 @@
 pragma solidity ^0.8.4;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+///Token holders will be able to destroy their tokens.
 import "openzeppelin-solidity/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "openzeppelin-solidity/contracts/security/Pausable.sol";
 import "openzeppelin-solidity/contracts/access/AccessControl.sol";
+
+///Without paying gas, token holders will be able to allow third parties to transfer from their account.
 import "openzeppelin-solidity/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
+/// @custom:security-contact shivangmitra8@gmail.com
 contract Krion is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Permit {
+    ///Flexible mechanism with a separate role for each privileged action. A role can have many authorized accounts.
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    ///Grant all the roles to the deployer and create an initial amount of tokens for the deployer.
     constructor() ERC20("Krion", "KRI") ERC20Permit("Krion") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(PAUSER_ROLE, msg.sender);
@@ -18,18 +25,22 @@ contract Krion is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Permit {
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
+    ///Privileged accounts will be able to pause the functionality marked as whenNotPaused. Useful for emergency response.
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
+    ///Privileged accounts will be able to unpause the functionality marked as whenNotPaused.
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
+    ///Privileged accounts will be able to create more supply.
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
+    ///Before transfering any amount from one account to another we check if this functionality is paused or not.
     function _beforeTokenTransfer(
         address from,
         address to,
--- contracts/KrionNFT.sol
+++ contracts/KrionNFT.sol
@@ -5,9 +5,14 @@ import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-solidity/contracts/security/Pausable.sol";
 import "openzeppelin-solidity/contracts/access/AccessControl.sol";
+
+///Token holders will be able to destroy their tokens.
 import "openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+///New tokens will be automatically assigned an incremental id.
 import "openzeppelin-solidity/contracts/utils/Counters.sol";
 
+/// @custom:security-contact shivangmitra8@gmail.com
 contract KrionNFT is ERC721, Pausable, AccessControl, ERC721Burnable {
     IERC20 public tokenAddress;
 
@@ -15,6 +20,7 @@ contract KrionNFT is ERC721, Pausable, AccessControl, ERC721Burnable {
 
     using Counters for Counters.Counter;
 
+    ///Flexible mechanism with a separate role for each privileged action. A role can have many authorized accounts.
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
@@ -27,35 +33,49 @@ contract KrionNFT is ERC721, Pausable, AccessControl, ERC721Burnable {
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function withdrawToken() public {
+    ///Only admin can withdraw the amount(ERC20 tokens) stored in the contract by minting new NFTs
+    function withdrawToken() public onlyRole(DEFAULT_ADMIN_ROLE) {
         tokenAddress.transfer(
             msg.sender,
             tokenAddress.balanceOf(address(this))
         );
     }
 
+    ///Buying NFT and transfering ERC20 tokens
+    function buy(address from, uint256 tokenId) public {
+        require(tokenAddress.balanceOf(msg.sender) >= rate);
+        tokenAddress.transferFrom(msg.sender, from, rate);
+        transferFrom(from, msg.sender, tokenId);
+    }
+
+    ///Selling NFT and transfering ERC20 tokens
     function sell(address to, uint256 tokenId) public {
         require(tokenAddress.balanceOf(to) >= rate);
         tokenAddress.transferFrom(to, msg.sender, rate);
         transferFrom(msg.sender, to, tokenId);
     }
 
+    ///Privileged accounts will be able to pause the functionality marked as whenNotPaused. Useful for emergency response.
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
+    ///Privileged accounts will be able to unpause the functionality marked as whenNotPaused.
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
 
+    ///Minting new NFT and transfering ERC20 tokens
     function safeMint() public {
         tokenAddress.transferFrom(msg.sender, address(this), rate);
 
+        ///New tokens will be automatically assigned an incremental id.
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
     }
 
+    ///Before transfering any amount from one account to another we check if this functionality is paused or not
     function _beforeTokenTransfer(
         address from,
         address to,
